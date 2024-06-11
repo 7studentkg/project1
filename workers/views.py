4097e51e1049449f6b6237f0eff5e34926184799
7studentkg <raimberdi.uulu.abdurahman@gmail.com>
2024-06-11 21:57:31 +0600

    l-l-l-l-commit

--- workers/views.py
+++ workers/views.py
@@ -108,7 +108,7 @@ class ClientDetail(RetrieveUpdateDestroyAPIView):
 
 
 class StandartSetPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 5
     page_size_query_param = 'page_size'
     max_page_size = 20
 
