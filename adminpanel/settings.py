8611cffa26c550624dd952dd01af27b98ba5dc72
7studentkg <raimberdi.uulu.abdurahman@gmail.com>
2024-06-13 16:59:50 +0600

    back_s_m

--- adminpanel/settings.py
+++ adminpanel/settings.py
@@ -158,12 +158,12 @@ USE_TZ = True
 
 
 # URL, по которому будут доступны статические файлы
-STATIC_URL = '/static/'
+STATIC_URL = '/back_static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'back_static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/back_media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'back_media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
